172b19cc01c91994479641e7bd4c71738e717299
cyj1322 <cyj1322@outlook.com>
2022-05-29 16:11:32 +0900

    sequelize migration 추가(User)

---
+++ app/lib/db/migrations/20220529024916-users.js
@@ -0,0 +1,54 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      userId: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: Sequelize.UUIDV4,
+        comment: '사용자 ID',
+      },
+      id: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        comment: '로그인 ID',
+      },
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        comment: '로그인 Password',
+      },
+      firstName: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        comment: '이름',
+      },
+      lastName: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        comment: '성',
+      },
+      nickName: {
+        type: Sequelize.STRING(20),
+        allowNull: false,
+        comment: '별명',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        comment: '생성 일시',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        comment: '수정 일시',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  },
+};
